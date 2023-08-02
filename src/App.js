08882856b94fdeff1007b0141ccd8a3db9dc694f
MonaKandoz <redbaird93@yahoo.com>
2023-08-02 04:00:48 +0300

    initialize tech-book-store

--- src/App.js
+++ src/App.js
@@ -1,25 +1,50 @@
-import logo from './logo.svg';
-import './App.css';
-
-function App() {
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="categories-container">
+      <div className="category-container">
+        <span
+          className="category-image"
+          style={{ backgroundImage: `url()` }}
+        ></span>
+        <span className="category-title">JavaScript</span>
+      </div>
+      <div className="category-container">
+        <span
+          className="category-image"
+          style={{ backgroundImage: `url()` }}
+        ></span>
+        <span className="category-title">PHP</span>
+      </div>
+      <div className="category-container">
+        <span
+          className="category-image"
+          style={{ backgroundImage: `url()` }}
+        ></span>
+        <span className="category-title">CSS</span>
+      </div>
+      <div className="category-container">
+        <span
+          className="category-image"
+          style={{ backgroundImage: `url()` }}
+        ></span>
+        <span className="category-title">React.JS</span>
+      </div>
+      <div className="category-container">
+        <span
+          className="category-image"
+          style={{ backgroundImage: `url()` }}
+        ></span>
+        <span className="category-title">Paython</span>
+      </div>
+      <div className="category-container">
+        <span
+          className="category-image"
+          style={{ backgroundImage: `url()` }}
+        ></span>
+        <span className="category-title">GraphQL</span>
+      </div>
     </div>
   );
-}
+};
 
 export default App;
